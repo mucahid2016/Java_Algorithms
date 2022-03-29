1fc1bbad9b1266109bcd69eb27b75af996ca09c1
mucahid2016 <81246450+mucahid2016@users.noreply.github.com>
2022-03-29 19:40:06 +0300

    s

--- src/DiskriminantBulma.java
+++ src/DiskriminantBulma.java
@@ -22,7 +22,16 @@ public class DiskriminantBulma {
         double diskriminant1 = (-b - (deltaKok)) / (2 * a);
         double diskriminant2 = (-b + (deltaKok)) / (2 * a);
 
-        System.out.println("1. Diskriminant Değeri : " + diskriminant1);
-        System.out.println("2. Diskriminant Değeri : " + diskriminant2);
+        if (delta>0){
+            System.out.println("1. Diskriminant Değeri : " + diskriminant1);
+            System.out.println("2. Diskriminant Değeri : " + diskriminant2);
+        }else if (delta==0){
+            double sıfırliKok=-b/2*a;
+            System.out.print("0' olduğunda kök : "+sıfırliKok);
+        }else {
+            System.out.println("0'dan aşagı ise kökü yoktur");
+        }
+
+
     }
 }
