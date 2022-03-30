14d467d4e8de9e7cca5e7f0f308070d15e03e22d
mucahid2016 <81246450+mucahid2016@users.noreply.github.com>
2022-03-30 09:44:00 +0300

    s

---
+++ src/SayiToplama.java
@@ -0,0 +1,9 @@
+public class SayiToplama {
+    public static void main(String[] args) {
+        int toplam = 0;
+        for (int i = 1; i <= 100; i++) {
+            toplam += i;
+        }
+        System.out.println("1-100 Arası Sayıların Toplamı : " + toplam);
+    }
+}
